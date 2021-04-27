cc0e0ef2ef536580b6a4f50c8345e7c0a3f5d6ca
Phi Ho <thivvnlh@gmail.com>
2021-04-27 20:44:22 +0700

    Update repo

--- src/services/file.js
+++
@@ -1,9 +0,0 @@
-import Promise from 'bluebird';
-import fs from 'fs';
-
-const readFileAsync = Promise.promisify(fs.readFile);
-const writeFileAsync = Promise.promisify(fs.writeFile);
-const unlinkAsync = Promise.promisify(fs.unlink);
-const fstat = Promise.promisify(fs.stat);
-
-export { readFileAsync, writeFileAsync, unlinkAsync, fstat }
